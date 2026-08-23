8068643427565147d7c6d3e8431cc1fd5eeea340
NadaFayez13 <naditafayez13@gmail.com>
2026-08-23 15:58:11 +0300

    Add initial FastAPI application with basic endpoints

---
+++ server.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+app = FastAPI()   
+
+@app.get("/")
+def say_hello():
+    return {"message": "Hello, World!"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok😉"}
+
+@app.get("/greet")
+def greet(name: str="Nadosha"):
+    return {"message": f"Hello, {name}🌹"}
+
+@app.get("/add")
+def add_numbers(a: int, b: int):
+    return {
+        "num1:": a,
+        "num2:": b,
+        "result": a + b}
+
